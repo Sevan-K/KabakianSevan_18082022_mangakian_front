8bf7c36da1d5fe6e2c16b06ef962b5d9c5b817d4
KABAKIAN <sevan.kabakian@capgemini.com>
2022-08-23 16:34:58 +0200

    feat: app-header styled

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
-@NgModule({ exports: [MatToolbarModule, MatIconModule] })
+@NgModule({ exports: [MatToolbarModule, MatIconModule, MatButtonModule] })
 export class MaterialModule {}
